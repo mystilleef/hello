681e54776daf2d4c16ae1a38ce2b82b5a2e00473
Lateef Alabi-Oki <mystilleef@gmail.com>
2020-10-26 17:14:19 -0400

    build: remove comments

--- stackghcidtest.sh
+++ stackghcidtest.sh
@@ -7,15 +7,6 @@ LIB_TARGET="${PACKAGE}:lib"
 TEST_TARGET="${PACKAGE}:test:${PACKAGE}-test"
 
 GHCI_OPTIONS="-w -fobject-code -fno-print-bind-result"
-#\
-#  -Weverything \
-#  -Wno-missing-import-lists \
-#  -Wno-missing-export-lists \
-#  -fno-safe-haskell \
-#  -Wno-unsafe \
-#  -Wno-safe \
-#  -j \
-#  -Wno-missing-local-signatures"
 
 STACK_GHCI_COMMAND="stack ghci $LIB_TARGET \
   --main-is $TEST_TARGET \
