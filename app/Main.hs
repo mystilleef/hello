3bd261247683579c3804dfbe99536303885b1903
Lateef Alabi-Oki <mystilleef@gmail.com>
2020-10-26 22:06:38 -0400

    feat: add input query

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,13 @@ module Main (module Main) where
 
 import DogsRule (dogs)
 import Hello (sayHello)
-import Prelude (IO, getLine)
+import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
+import Prelude (IO, getLine, putStr)
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering
+  putStr "Please input your name: "
   name <- getLine
   sayHello name
   dogs
